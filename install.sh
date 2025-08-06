a0c26e516d57c175c4b3b70d9ff7d981014251c7
ivanpondic-ship-it <ivanpondic@gmail.com>
2025-08-06 14:41:57 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -31,43 +31,48 @@ while true; do
   echo "=============================="
   echo "   Thor Stream PANEL Menu"
   echo "=============================="
-  echo " 1) Install"
-  echo " 2) Update"
-  echo " 3) Update GeoLite"
-  echo " 4) Close SSH"
+  echo " 1) Install Main Server"
+  echo " 2) Install Load Balancer"
+  echo " 3) Update Main Server"
+  echo " 4) Update GeoLite"
+  echo " 5) Exit Installer"
   echo "=============================="
   read -p "Select: " choice
 
   case $choice in
     1)
-      echo "Installing panel..."
+      echo "Installing Main Server..."
       mkdir -p $INSTALL_DIR
       wget -q --no-check-certificate "https://drive.google.com/uc?export=download&id=1PMi7EX9JE0JN5Jl7HXMQfzUBT6VvRVHF" -O thor_installer.zip
       unzip -o thor_installer.zip -d $INSTALL_DIR
-      echo "✅ Installation completed."
+      echo "✅ Main Server Installation completed."
       read -p "[ENTER] Return to menu..."
       ;;
     2)
-      echo "Updating panel..."
-      wget -q --no-check-certificate "https://drive.google.com/uc?export=download&id=1rRJ2Mo_RNLNeAqv6pU1Be1MpUKcVf3N6" -O thor_update.zip
-      unzip -o thor_update.zip -d $INSTALL_DIR
-      echo "✅ Update completed."
+      echo "Installing Load Balancer..."
+      mkdir -p $INSTALL_DIR
+      wget -q --no-check-certificate "https://drive.google.com/uc?export=download&id=13fEhJEJx-E2wCErUEd1WtyjY4Wz2L8nt" -O thor_lb_installer.zip
+      unzip -o thor_lb_installer.zip -d $INSTALL_DIR
+      echo "✅ Load Balancer Installation completed."
       read -p "[ENTER] Return to menu..."
       ;;
     3)
+      echo "Updating Main Server..."
+      wget -q --no-check-certificate "https://drive.google.com/uc?export=download&id=1rRJ2Mo_RNLNeAqv6pU1Be1MpUKcVf3N6" -O thor_update.zip
+      unzip -o thor_update.zip -d $INSTALL_DIR
+      echo "✅ Main Server Update completed."
+      read -p "[ENTER] Return to menu..."
+      ;;
+    4)
       echo "Updating GeoLite2..."
       mkdir -p $INSTALL_DIR/geo
       wget -q https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz -O - | tar -xz -C $INSTALL_DIR/geo --strip-components=1
       echo "✅ GeoLite updated."
       read -p "[ENTER] Return to menu..."
       ;;
-    4)
-      read -p "Close SSH session? (y/n): " close_ssh
-      if [[ "$close_ssh" =~ ^[Yy]$ ]]; then
-        echo "Closing SSH..."
-        sleep 1
-        pkill -KILL -u $(whoami)
-      fi
+    5)
+      echo "Exiting installer..."
+      break
       ;;
     *)
       echo "Invalid selection."
